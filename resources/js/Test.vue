78dba6d43a18763e8f4415bc330141d21b3928ba
Jérémy Gfeller <36196039+JeremyGfeller@users.noreply.github.com>
2021-11-17 15:13:50 +0100

    Get data from the api

--- resources/js/Test.vue
+++ resources/js/Test.vue
@@ -5,7 +5,23 @@
 <script>
 
 export default {
-  //
+  data() {
+    return {
+      categories: [],
+    }
+  },
+  mounted() {
+    this.getCategories();
+  },
+  methods : {
+    getCategories() {
+      axios.get('/api/categories').then((response) => {
+        this.categories = response.data.data
+      }).catch(function (error) {
+        console.log(error);
+      })
+    }
+  }
 }
 
 </script>
