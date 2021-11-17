65251c961559faaee4eac26bfac9f64ec4552242
Jérémy Gfeller <36196039+JeremyGfeller@users.noreply.github.com>
2021-11-17 15:04:08 +0100

    Import only the vue component

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,10 +1,5 @@
 @extends('layouts.app')
 
 @section('content')
-  <div class="card my-4">
-    <div class="card-body">
-      Categories
-      <test></test>
-    </div>
-  </div>
+  <test></test>
 @endsection
